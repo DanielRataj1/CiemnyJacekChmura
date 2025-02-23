054d4bd298ed4b0e7d2efb8ed3835b9a17de76c4
DanielRataj1 <127019047+DanielRataj1@users.noreply.github.com>
2025-02-23 14:50:08 +0100

    CJ1
    "Przerobić nazwy funkcji z języka polskiego na język angielski / CJ1" -> zadanie zrealizowane, cały projekt (z wyłączeniem informacji o wygranej  / porażce) przepisany na język angielski.

--- script.js
+++ script.js
@@ -1,77 +1,77 @@
-let dealerSuma = 0;
-let graczSuma = 0;
+let dealerSum = 0;
+let playerSum = 0;
 
 let dealerAceCount = 0;
-let graczAceCount = 0;
+let playerAceCount = 0;
 
 let hidden;
-let talia;
+let deck;
 
 let canHit = true; // pozwala graczowi dobrać kartę jeśli yourSum <= 21
 
 window.onload = function(){
-    zbudujKarty();
-    tasowanieKart();
-    startGry();
+    buildDeck();
+    shuffleDeck();
+    startGame();
 }
 
-function zbudujKarty() {
-    let numer = ["A","2","3","4","5","6","7","8","9","10","J","D","K"]; /// wszystkie karty 
-    let kolor = ["K","P","C","T"]; /// K - karo, P - pik, C - czerwo, T - trefl
-    talia = [];
+function buildDeck() {
+    let number = ["A","2","3","4","5","6","7","8","9","10","J","D","K"]; /// wszystkie karty 
+    let colour = ["K","P","C","T"]; /// K - karo, P - pik, C - czerwo, T - trefl
+    deck = [];
  //   let karty = ["AK", "2K"...];  
    
- for (let i = 0; i < kolor.length; i++) {
-        for (let j = 0; j < numer.length; j++) {
-            talia.push(numer[j] + "-" + kolor[i]);
+ for (let i = 0; i < colour.length; i++) {
+        for (let j = 0; j < number.length; j++) {
+            deck.push(number[j] + "-" + colour[i]);
         }
     }
-    // console.log(talia);
+    // console.log(deck);
 }
 
-function tasowanieKart() {
-    for (let i = 0; i < talia.length; i++) {
-        let j = Math.floor(Math.random() * talia.length);
-        let temp = talia[i];
-        talia[i] = talia[j];
-        talia[j] = temp;
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
 
     }
-    console.log(talia);
+    console.log(deck);
 }
 
-function startGry() {
-    hidden = talia.pop();
-    dealerSuma += getValue(hidden);
+function startGame() {
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
     dealerAceCount += checkAce(hidden);
     // console.log(hidden);
-    // console.log(dealerSuma);
+    // console.log(dealerSum);
     
-    while (dealerSuma < 17) {
-        let kartaImg = document.createElement("img");
-        let karta = talia.pop();
-        kartaImg.src = "./karty_OG_Projcet/" + karta + ".png";
-        dealerSuma += getValue(karta);
-        dealerAceCount += checkAce(karta);
-        document.getElementById("dealer-karty").append(kartaImg);
+    while (dealerSum < 17) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./karty_OG_Projcet/" + card + ".png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
     }
-    console.log(dealerSuma);
+    console.log(dealerSum);
     
     for (let i = 0; i < 2; i++) {
-        let kartaImg = document.createElement("img");
-        let karta = talia.pop();
-        kartaImg.src = "./karty_OG_Projcet/" + karta + ".png";
-        graczSuma += getValue(karta);
-        graczAceCount += checkAce(karta);
-        document.getElementById("gracz-karty").append(kartaImg);
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./karty_OG_Projcet/" + card + ".png";
+        playerSum += getValue(card);
+        playerAceCount += checkAce(card);
+        document.getElementById("player-cards").append(cardImg);
     }
 
-    console.log(graczSuma);
+    console.log(playerSum);
     document.getElementById("hit").addEventListener("click", hit);
     document.getElementById("stay").addEventListener("click", stay);
     document.getElementById("reset").addEventListener("click", reset);
-    document.getElementById("gracz-suma").innerText = graczSuma;
-    document.getElementById("dealer-suma").innerText = "?";
+    document.getElementById("player-sum").innerText = playerSum;
+    document.getElementById("dealer-sum").innerText = "?";
 }
 
 function hit() {
@@ -79,38 +79,38 @@ function hit() {
         return;
     }
 
-    let kartaImg = document.createElement("img");
-    let karta = talia.pop();
-    kartaImg.src = "./karty_OG_Projcet/" + karta + ".png";
-    graczSuma += getValue(karta);
-    graczAceCount += checkAce(karta);
-    document.getElementById("gracz-karty").append(kartaImg);
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./karty_OG_Projcet/" + card + ".png";
+    playerSum += getValue(card);
+    playerAceCount += checkAce(card);
+    document.getElementById("player-cards").append(cardImg);
 
-    while (graczSuma > 21 && graczAceCount > 0) {
-        graczSuma -= 10;
-        graczAceCount -= 1;
+    while (playerSum > 21 && playerAceCount > 0) {
+        playerSum -= 10;
+        playerAceCount -= 1;
     }
 
-    while (dealerSuma > 21 && dealerAceCount > 0) {
-        dealerSuma -= 10;
+    while (dealerSum > 21 && dealerAceCount > 0) {
+        dealerSum -= 10;
         dealerAceCount -= 1;
     }
 
     // Update the player's total display
-    document.getElementById("gracz-suma").innerText = graczSuma;
+    document.getElementById("player-sum").innerText = playerSum;
 
-    if (graczSuma > 21) {
+    if (playerSum > 21) {
         canHit = false;
         let message = "Przegrałeś!";
-        document.getElementById("wynik").innerText = message;
-        document.getElementById("dealer-suma").innerText = dealerSuma;
+        document.getElementById("result").innerText = message;
+        document.getElementById("dealer-sum").innerText = dealerSum;
         document.getElementById("hidden").src = "./karty_OG_Projcet/" + hidden + ".png";
-        document.getElementById("wynik").innerText = message;
+        document.getElementById("result").innerText = message;
     }
 }
 
-function getValue(karta) {
-    let data = karta.split("-");
+function getValue(card) {
+    let data = card.split("-");
     let value = data[0];
     
     if (isNaN(value)) {
@@ -122,49 +122,49 @@ function getValue(karta) {
         return parseInt(value);
     }
 
-function checkAce(karta) {
-    if (karta[0] == "A") {
+function checkAce(card) {
+    if (card[0] == "A") {
         return 1;
     }
     return 0;
 }
 
-function reduceAce(graczSuma, graczAceCount) {
-    while (graczSuma > 21 && graczAceCount > 0) {
-        graczSuma -= 10;
-        graczAceCount -= 1;
+function reduceAce(playerSum, playerAceCount) {
+    while (playerSum > 21 && playerAceCount > 0) {
+        playerSum -= 10;
+        playerAceCount -= 1;
     }
-    return graczSuma;
+    return playerSum;
 }
 
 function stay() {
-    dealerSuma = reduceAce(dealerSuma, dealerAceCount);
-    graczSuma = reduceAce(graczSuma, graczAceCount);
+    dealerSum = reduceAce(dealerSum, dealerAceCount);
+    playerSum = reduceAce(playerSum, playerAceCount);
 
     canHit = false;
     document.getElementById("hidden").src = "./karty_OG_Projcet/" + hidden + ".png";
 
     let message = "";
-    if (graczSuma > 21) { 
+    if (playerSum > 21) { 
         message = "Przegrałeś!"
     }
-    else if (dealerSuma > 21){
+    else if (dealerSum > 21){
         message = "Wygrałeś!";
     }
     // gracz i dealer mają sume kart mniejsza na 21 
-    else if (graczSuma == dealerSuma) {
+    else if (playerSum == dealerSum) {
         message = "Remis!";
     }
-    else if (graczSuma > dealerSuma){
+    else if (playerSum > dealerSum){
         message = "Wygrałeś!";
     }
-    else if (graczSuma < dealerSuma){
+    else if (playerSum < dealerSum){
         message = "Przegrałeś!";
     }
     
-    document.getElementById("gracz-suma").innerText = graczSuma;
-    document.getElementById("dealer-suma").innerText = dealerSuma;
-    document.getElementById("wynik").innerText = message;
+    document.getElementById("player-sum").innerText = playerSum;
+    document.getElementById("dealer-sum").innerText = dealerSum;
+    document.getElementById("result").innerText = message;
 }
 
 function reset() {
